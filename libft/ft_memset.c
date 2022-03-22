a8fb714fda4b45f8b24d7320003be3d3919f07f0
Do hyun Paek <dopaek@c4r8s8.42seoul.kr>
2022-03-22 15:40:35 +0900

    ft_memset.c

---
+++ libft/ft_memset.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dopaek <dopaek@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/22 14:18:33 by dopaek            #+#    #+#             */
+/*   Updated: 2022/03/22 14:18:34 by dopaek           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	unsigned char	*cpy;
+	unsigned char	n;
+	size_t			i;
+
+	cpy = b;
+	n = c;
+	i = -1;
+	while (++i < len)
+		*cpy++ = n;
+	return (b);
+}
