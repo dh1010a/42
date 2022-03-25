7ea1e93104888653849ec76bfafb25d6cd403417
dh1010a <=>
2022-03-25 21:03:02 +0900

    ft_memcpy

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -2,5 +2,16 @@
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	
+	unsigned char	*cpy_dest;
+	unsigned char	*cpy_src;
+	size_t			i;
+
+	if (!dest && !src)
+		return (0);
+	cpy_dest = dest;
+	cpy_src = (unsigned char *)src;
+	i = 0;
+	while(i++ < n)
+		*cpy_dest++ = *cpy_src++;
+	return (dest);
 }
